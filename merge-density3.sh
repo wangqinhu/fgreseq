b2ecea10d0bdc55155b47d8440ac915c35c31067
Qinhu Wang <qinhu.wang@icloud.com>
2015-11-20 00:24:31 +0800

    update average density calculation

--- merge-density3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#$ -cwd
-#$ -N merge_density.winstep3
-#$ -j y
-
-# Sum eff.density.winstep files
-cd data/density.winstep/25000
-paste <(cut -f1,2,3 YL-1.density.winstep.txt) <(paste <(cut -f4 YL-1.density.winstep.txt) <(cut -f4 HN9-1.density.winstep.txt) <(cut -f4 HN-Z6.density.winstep.txt) | awk '{print $1+$2+$3}') > density.winstep.txt
