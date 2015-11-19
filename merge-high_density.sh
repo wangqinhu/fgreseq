083ea0fb50937909cc84a49201215db374993df8
Qinhu Wang <qinhu.wang@icloud.com>
2015-11-20 00:43:13 +0800

    remove unused files

--- merge-high_density.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#$ -cwd
-#$ -N merge-high_density
-#$ -j y
-
-# Sum eff.high.density files
-cd data/density/25000
-paste <(cut -f1,2,3 YL-1.high.density.txt) <(paste <(cut -f4 YL-1.high.density.txt) <(cut -f4 HN9-1.high.density.txt) <(cut -f4 HN-Z6.high.density.txt) <(cut -f4 GJ33.high.density.txt) <(cut -f4 FJ1-38.high.density.txt) | awk '{print $1+$2+$3+$4+$5}') > high.density.txt
