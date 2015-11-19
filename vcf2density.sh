0a31e349c0c1d36f630e5ba681d1e44a6b333d06
Qinhu Wang <qinhu.wang@icloud.com>
2015-11-20 00:04:53 +0800

    update vcf2density

--- vcf2density.sh
+++ vcf2density.sh
@@ -10,10 +10,9 @@
 
 mkdir -p data/density/$1
 
-reseq=("HN9-1" "HN-Z6" "YL-1" "GJ33" "FJ1-38")
+reseq=("HN9-1" "HN-Z6" "YL-1" "CS3005" "GZ3639")
 
 id=${reseq[$(expr $SGE_TASK_ID - 1)]}
 
 # Covert vcf files to density files
-perl ./vcf2density.pl data/eff/$id.ann.vcf data/density/$1/$id.density.txt $1
-
+perl ./vcf2density.pl data/vcf_filtered/$id.vcf data/density/$id.density.txt $1
