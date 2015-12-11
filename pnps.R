5706f93f78f861cdf9ec50b09942211e9711916e
Qinhu Wang <qinhu.wang@icloud.com>
2015-12-11 21:50:39 +0800

    add classpro for pnps plot

--- pnps.R
+++ pnps.R
@@ -3,8 +3,8 @@ library(plotrix)
 
 x<-read.table("data/pnps/pnps.tsv")
 
-pdf("data/pnps/positive.pdf", 7.4, 2.8)
-layout(matrix(c(1,2,3),1,3,byrow = TRUE), c(1.0,1.3,0.8), c(1.2,0.8,1.2),respect = T)
+pdf("data/pnps/positive.pdf", 6, 6)
+layout(matrix(c(1,2,3,4),2,2,byrow = TRUE), c(1,1), c(1,1),respect = T)
 
 #############
 # positive 1 #
@@ -35,7 +35,7 @@ system("./isc data/2speed/list.slow data/pnps/positive1.id.txt > data/pnps/posit
 system("./isc data/2speed/list.fast data/pnps/positive.txt > data/pnps/positive.fast")
 system("./isc data/2speed/list.slow data/pnps/positive.txt > data/pnps/positive.slow")
 
-par(mar=c(4.25,4,0.5,2))
+par(mar=c(4.25,4,1.5,0.5))
 z<-hist(pn_ps, breaks = 50, plot=F)
 xt<-z$breaks
 gap.barplot(z$density,
@@ -55,7 +55,27 @@ axis.break(2,from,breakcol="honeydew",style="gap")
 axis.break(2,from*(1+0.02),breakcol="black",style="zigzag")
 axis.break(4,from*(1+0.02),breakcol="black",style="zigzag")
 
-mtext("A", adj=0.018, line=-2.0, outer=T, cex=1.0)
+mtext("a", adj=0.01, line=-2, outer=T)
+
+############
+# classpro #
+############
+fast<-6092
+slow<-8072
+positive<-read.table("data/classpro/positive.tsv")
+p<-as.matrix(positive)
+p[1,]<-p[1,]/fast
+p[2,]<-p[2,]/slow
+par(mar=c(4.25,6,1.5,0.5))
+barplot(prop.table(p,2),
+        col = c("yellow","purple"),
+        ylab = "Ratio of subgenome",
+        xlim = c(0,7),
+        cex.names = 0.75
+)
+legend(5, 1, rev(rownames(p)), fill = c("purple","yellow"),cex=0.8)
+mtext("b", adj=0.56, line=-2, outer=T)
+
 
 #############
 # positive 2 #
@@ -68,7 +88,7 @@ for (i in seq(1,10)) {
 }
 
 # plot all
-par(mar=c(4.25,4,0.5,2))
+par(mar=c(4.25,4,1.5,0.5))
 hist2(log2(pss/10),log2(pns/10), key = vkey, key.args = list(x=-1,y=-15.5,stretch = 1), nx=100, xlim=c(-16,0), ylim=c(-16,0), xlab = "log2(pS)", ylab="log2(pN)", colFn = jet)
 abline(a = 0, b = 1, col = 2)
 # get positive2
@@ -96,14 +116,14 @@ for (i in seq(0,9)) {
 }
 points(log2(ps_f/10),log2(pn_f/10),pch=20,cex=0.8,col="yellow")
 legend("topleft", c("fast","slow", "pN=pS"), pch = c(20,20,-1), lty=c(-1,-1,1), bg ="lightgreen", col = c("yellow", "purple", "red"))
-mtext("B", adj=0.335, line=-2.0, outer=T, cex=1.0)
+mtext("c", adj=0.01, line=-20, outer=T)
 
 ##############
 # boxplot pN #
 ##############
 y<-list(pn_s/10,pn_f/10)
 names(y)<-c("slow","fast")
-par(mar=c(4.25,4,0.5,0))
+par(mar=c(4.25,6,1.5,1))
 boxplot(y,col=c("purple","yellow"),ylab="pN")
-mtext("C", adj=0.75, line=-2.0, outer=T, cex=1.0)
+mtext("d", adj=0.56, line=-20, outer=T)
 dev.off()
