94244c8dfa3c998452b63333446ef3d71dd3875d
Qinhu Wang <qinhu.wang@icloud.com>
2015-12-15 10:20:37 +0800

    update density

--- merge-density.sh
+++ merge-density.sh
@@ -2,4 +2,5 @@
 
 # density_directory
 dd="./data/density"
-paste <(cut -f1,2,3 $dd/YL-1.density.txt) <(paste <(cut -f4 $dd/YL-1.density.txt) <(cut -f4 $dd/HN9-1.density.txt) <(cut -f4 $dd/HN-Z6.density.txt) <(cut -f4 $dd/CS3005.density.txt)| awk '{print ($1+$2+$3+$4)/4}') > $dd/density.txt
+paste <(cut -f1,2,3 $dd/YL-1.density.txt) <(paste <(cut -f4 $dd/YL-1.density.txt) <(cut -f4 $dd/HN9-1.density.txt) <(cut -f4 $dd/HN-Z6.density.txt) <(cut -f4 $dd/CS3005.density.txt) | awk '{print ($1+$2+$3+$4)/4}') > $dd/density.txt
+paste <(cut -f1,2,3 $dd/YL-1.density.txt) <(paste <(cut -f4 $dd/YL-1.density.txt) <(cut -f4 $dd/HN9-1.density.txt) <(cut -f4 $dd/HN-Z6.density.txt) <(cut -f4 $dd/CS3005.density.txt) <(cut -f4 $dd/FG1.density.txt) <(cut -f4 $dd/FG2.density.txt) | awk '{print ($1+$2+$3+$4+$5+$6)/6}') > $dd/density6.txt
--- vcf2density.sh
+++ vcf2density.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#$ -t 1-5
-#$ -cwd
-#$ -N vcf2density
-#$ -j y
+mkdir -p data/density
 
-# usage
-# qsub *.sh window_size
+cp data/wgs/*.vcf data/vcf_filtered/
 
-mkdir -p data/density/$1
-
-reseq=("HN9-1" "HN-Z6" "YL-1" "CS3005" "GZ3639")
-
-id=${reseq[$(expr $SGE_TASK_ID - 1)]}
+strains=("HN9-1" "HN-Z6" "YL-1" "CS3005" "GZ3639" "FG1" "FG2")
 
 # Covert vcf files to density files
-perl ./vcf2density.pl data/vcf_filtered/$id.vcf data/density/$id.density.txt $1
+for strain in  ${strains[@]}; do
+	perl ./vcf2density.pl data/vcf_filtered/$strain.vcf data/density/$strain.density.txt
+done
