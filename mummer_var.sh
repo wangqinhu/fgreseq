6e3c160188ecb70795ee326da697d55a4e1200ef
Qinhu Wang <qinhu.wang@icloud.com>
2015-12-15 10:11:12 +0800

    keep case consistently

--- mummer_var.sh
+++ mummer_var.sh
@@ -5,25 +5,25 @@ abacas.1.3.1.pl -r data/refseq/merged.one.fa -q data/wgs/FG1.fa -p nucmer -m -o 
 abacas.1.3.1.pl -r data/refseq/merged.one.fa -q data/wgs/FG2.fa -p nucmer -m -o data/wgs/FG2o
 
 # alignment
-nucmer --prefix=fg1 data/refseq/FG.RR.27.genome.fa data/wgs/FG1o.MULTIFASTA.fa
-nucmer --prefix=fg2 data/refseq/FG.RR.27.genome.fa data/wgs/FG2o.MULTIFASTA.fa
+nucmer --prefix=FG1 data/refseq/FG.RR.27.genome.fa data/wgs/FG1o.MULTIFASTA.fa
+nucmer --prefix=FG2 data/refseq/FG.RR.27.genome.fa data/wgs/FG2o.MULTIFASTA.fa
 
 mv *.delta data/wgs/
 
 # call var
-show-snps -T data/wgs/fg1.delta > data/wgs/fg1.snps
-show-snps -T data/wgs/fg2.delta > data/wgs/fg2.snps
+show-snps -T data/wgs/FG1.delta > data/wgs/FG1.snps
+show-snps -T data/wgs/FG2.delta > data/wgs/FG2.snps
 
 # covert to vcf
 
 # vcf
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa data/wgs/fg1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/fg1.vcf
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa data/wgs/fg2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/fg2.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa data/wgs/FG1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/FG1.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa data/wgs/FG2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/FG2.vcf
 
 # snps
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t SNP data/wgs/fg1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/fg1.snps.vcf
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t SNP data/wgs/fg2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/fg2.snps.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t SNP data/wgs/FG1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/FG1.snps.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t SNP data/wgs/FG2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/FG2.snps.vcf
 
 # indels
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t INDEL data/wgs/fg1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/fg1.indels.vcf
-./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t INDEL data/wgs/fg2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }'> data/wgs/fg2.indels.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t INDEL data/wgs/FG1.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }' > data/wgs/FG1.indels.vcf
+./mummer2Vcf.pl -f data/refseq/FG.RR.27.genome.fa -t INDEL data/wgs/FG2.snps | awk '{print $1 "\t" $2 "\t.\t" $3 "\t" $4 "\t.\t.\t" $5 }'> data/wgs/FG2.indels.vcf
