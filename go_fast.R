e12042edf200dff747bc9563837270851930c72f
Qinhu Wang <qinhu.wang@icloud.com>
2015-12-15 18:19:16 +0800

    update go_fast plot

--- go_fast.R
+++ go_fast.R
@@ -23,11 +23,11 @@ axis(1,at=seq(0,max(gof)+1,8))
 abline(h=seq(0.5,50.5,1),lty=3,col="grey75",lwd=0.6)
 abline(v=seq(-4,max(gof)+1,2),lty=3,col="grey75",lwd=0.6)
 rect(max(gof)+1.2,1-0.5,125.8,3-0.5,col="#CCCCFF", border = "white")
-rect(max(gof)+1.2,3-0.5,125.8,15-0.5,col="#FFFFCC", border = "white")
-rect(max(gof)+1.2,15-0.5,125.8,19-0.5,col="#FFCCCC", border = "white")
+rect(max(gof)+1.2,3-0.5,125.8,17-0.5,col="#FFFFCC", border = "white")
+rect(max(gof)+1.2,17-0.5,125.8,21-0.5,col="#FFCCCC", border = "white")
 
 points(gof,seq(1,length(go$GOID)),
-     col=jet(18)[rank(go$T)],
+     col=jet(20)[rank(go$T)],
      pch=16,
      cex=0.5+log2(gofold)*0.5
 )
@@ -37,8 +37,8 @@ text(max(gof)+2,seq(1,length(go$GOID)),paste(go$GOID,go$Term),cex=0.6,adj = 0)
 abline(v=119,col="white", lwd=2)
 abline(v=max(gof)+1)
 text(122,1.5,"CC",cex=0.6)
-text(122,8.5,"MF",cex=0.6)
-text(122,16.5,"BP",cex=0.6)
+text(122,9.5,"MF",cex=0.6)
+text(122,18.5,"BP",cex=0.6)
 box()
 
 
@@ -59,7 +59,7 @@ plot(c(1:5),rep(1,5),
      ylab="",
      xlab="",
      xlim=c(1,5.2),
-     axes = F,pch=16, cex=0.5+fivenum(log2(gofold)*0.45),col="green")
+     axes = F,pch=16, cex=0.5+fivenum(log2(gofold)*0.5),col="green")
 box()
 text(3,-0.1,"Fold change", adj=0.5)
 
