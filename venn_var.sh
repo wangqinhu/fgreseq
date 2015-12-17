50f42d45a99ead0c2cb967d1ff20dc072f4d6df0
Qinhu Wang <qinhu.wang@icloud.com>
2015-12-17 14:55:54 +0800

    remove expired venn scripts

--- venn_var.sh
+++ venn_var.sh
@@ -21,36 +21,4 @@ for strain in  ${strains[@]}; do
 	tabix -p vcf data/vcf_filtered_gi/$strain.vcf.gz
 done
 
-vcf-compare data/vcf_filtered_gi/*.vcf.gz | grep ^VN | cut -f 2- > data/venn_var/vcf.cmp.txt
-
-cat data/venn_var/vcf.cmp.txt | ./fmt_vcfcmp.pl - > data/venn_var/cmpid.txt
-
-cat <<'RSCRIPT' > venn_var.R 
-library("VennDiagram")
-# Reference five-set diagram
-venn.plot <- draw.quintuple.venn(
-RSCRIPT
-
-sh cmpid2vennd.sh >> venn_var.R
-
-cat <<'RSCRIPT' >> venn_var.R 
-  category = c("YL-1", "HN9-1", "HN-Z6", "GZ3639", "CS3005"),
-  fill = c("dodgerblue", "goldenrod1", "darkorange1", "seagreen3", "orchid3"),
-  cat.col = c("dodgerblue", "goldenrod1", "darkorange1", "seagreen3", "orchid3"),
-  #cat.cex = 1,
-  margin = 0.05,
-  cex = c(1, 1, 1, 1, 1, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1, 0.8, 
-          1, 0.55, 1, 0.55, 1, 0.55, 1, 0.55, 1, 0.55, 1, 1, 1, 1, 1, 1),
-  ind = TRUE,
-  col="white",
-  cat.fontfamily="ArialMT",
-  fontfamily="ArialMT"
-);
-
-# Writing to file
-pdf("data/venn_var/venn.pdf",6.5,6.5)
-grid.draw(venn.plot)
-dev.off()
-RSCRIPT
-
-R CMD BATCH ./venn_var.R
+# call ivenn next
