e1c6425a040146afd72ef79043713d1d7af9ceea
Qinhu Wang <qinhu.wang@icloud.com>
2016-01-06 18:11:21 +0800

    fix a typo

--- merge_bam_rnaseq.sh
+++ merge_bam_rnaseq.sh
@@ -6,5 +6,5 @@
 
 db="../F.graminearum/data/rnaseq/bam"
 
-samtools merge -r $db/PH1_6d.merged.bam $db/PH1_6d_1.sorted.bam $db/PH1_6d_1.sorted.bam
+samtools merge -r $db/PH1_6d.merged.bam $db/PH1_6d_1.sorted.bam $db/PH1_6d_2.sorted.bam
 samtools merge -r $db/PH1_CM.merged.bam $db/PH1_CM_1.sorted.bam $db/PH1_CM_2.sorted.bam
